fa837846d538b35b980a3f6abe38cc58abda50ed
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-03 22:55:29 -0500

    display some data on a modal

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,4 @@
+import './styles/resets.scss';
+import './styles/modal.scss';
+
 import { handleSubmit } from './js/app';
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,10 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const location = document.querySelector('.location');
+const date = document.querySelector('.date');
+const tempHTML = document.querySelector('.temp');
+const img = document.querySelector('.img-location');
+
 const handleSubmit = async (e) => {
   e.preventDefault();
   const locationValue = document.getElementById('location').value;
@@ -8,8 +15,9 @@ const handleSubmit = async (e) => {
     body: JSON.stringify({ location: locationValue }),
   });
   const coordinates = await res.json();
-  getWeather(coordinates);
-  getImage(locationValue);
+  const temperature = await getWeather(coordinates);
+  const imageUrl = await getImage(locationValue);
+  displayData(temperature, imageUrl);
 };
 
 const getWeather = async ({ lng, lat }) => {
@@ -21,6 +29,7 @@ const getWeather = async ({ lng, lat }) => {
   const weatherData = await res.json();
   const { temp } = weatherData;
   console.log(temp);
+  return temp;
 };
 
 const getImage = async (location) => {
@@ -32,8 +41,25 @@ const getImage = async (location) => {
 
   const imageUrl = await res.json();
   console.log(imageUrl);
+  return imageUrl;
+};
+
+const displayData = (temp, imageUrl) => {
+  console.log('from display data', temp, imageUrl);
+  tempHTML.textContent = `Temperature: ${temp}`;
+  img.src = `${imageUrl.imageUrl}`;
+  // img.src =
+  //   'https://pixabay.com/get/gbad0e3e9aa9ad8d6015ebb58030c8896d0a4a7868472f419438afa763ff82fbcfd2e3665ed024886fdb329c161ca56c3332ef3de9ab160ad5f9521c02a197e95_640.jpg';
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 document.getElementById('form').addEventListener('submit', handleSubmit);
+document.body.addEventListener('click', closeModal);
 
 export { handleSubmit };

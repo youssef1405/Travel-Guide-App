c9733eca0b260468fead59364a8685dca6bbd6de
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-04 17:31:45 -0500

    style the form

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,6 @@
 import './styles/resets.scss';
+import './styles/base.scss';
 import './styles/modal.scss';
+import './styles/header.scss';
 
 import { handleSubmit } from './js/app';
--- webpack.dev.js
+++ webpack.dev.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 

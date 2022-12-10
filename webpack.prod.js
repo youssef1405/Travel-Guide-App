58536e3b0d346dcc89b5b8bc1adda452c24b5939
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-12-10 11:20:08 -0500

    Add service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -39,5 +40,6 @@ module.exports = {
       template: './src/client/views/index.html',
     }),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
